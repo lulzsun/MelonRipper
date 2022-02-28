e7e8b42eb81866bc80aff2bcfd626c52bb90d028
lognoion <lognoion@gmail.com>
2022-02-28 22:03:19 +0100

    fixed some variables, because they were change on the website

--- js/melonripper.js
+++ js/melonripper.js
@@ -1,12 +1,20 @@
 // define global variables
 var imageArray = [];
 var isSaving = false;
+var tenth = 0;
+var pageTotal = 0;
 
 // create an observer instance, in order to detect page changes
 const observer = new MutationObserver(function(mutations) {
+
+
     for(let mutation of mutations) {
         if (mutation.type === 'attributes' && mutation.attributeName === 'class' && mutation.target.className.includes('view-sheet-focus')) {
+
+
+
             savePages(mutation.target.firstChild.nextSibling.childNodes);
+
         }
     }
 });
@@ -16,16 +24,22 @@ const observer = new MutationObserver(function(mutations) {
     var target = document.body.getElementsByClassName('flipsnap')[0];
     var firstPage = document.body.getElementsByClassName('view-sheet-focus')[0];
 
+
+
     if(target != null) {
         // init imageArray length
-        var pageTotal = parseInt(document.body.getElementsByClassName('var-page-total')[0].textContent);
+        pageTotal = parseInt(document.body.getElementsByClassName('slider-label')[0].textContent.split('/')[1]);
         imageArray = Array.apply(null, Array(pageTotal)).map(function () {});
 
         // delete view-protection, just for better visual
-        document.body.getElementsByClassName('view-protection')[0].remove();
+        var doc = document.body.getElementsByClassName('view-protection')[0].remove();
+
+
 
         savePages(firstPage.firstChild.nextSibling.childNodes);
+
         observer.observe(target, { attributes: true, childList: true, subtree: true });
+
     } else {
         setTimeout(init, 1000);
     }
@@ -33,27 +47,37 @@ const observer = new MutationObserver(function(mutations) {
 
 // get images from canvases that are in focus (visible to the reader) and save to global var
 function savePages(pages) {
-    var pageIndex = parseInt(document.body.getElementsByClassName('var-page-current')[0].textContent)-1;
 
-    for(var i = pages.length - 1; i >= 0; i--) { // loop backwards, because manga reads right to left 
+    var pageIndex = parseInt(document.body.getElementsByClassName('slider-label')[0].textContent.split('/')[0])-1;
+
+    for(var i = pages.length - 1; i >= 0; i--) { // loop backwards, because manga reads right to left
         if(pages[i].tagName === 'CANVAS') {
             //save image as base64
             imageArray[pageIndex] = pages[i].toDataURL().replace(/^data:image\/(png);base64,/, '');
-            console.log(`[MelonRipper] Saved page ${pageIndex+1} of ${imageArray.length}`); 
+
+            console.log(`[MelonRipper] Saved page ${pageIndex+1} of ${imageArray.length}`);
             pageIndex++;
+
         }
+
+            if(pageIndex % 10 == 0){
+                saveToZip(tenth, pageIndex);
+                tenth = pageIndex;
+                imageArray = Array.apply(null, Array(pageTotal)).map(function () {});
+            }
     }
 }
 
+
 // save all images to a zip using the JSZip lib
-function saveToZip() {
+function saveToZip(start, end) {
     console.log('[MelonRipper] Preparing to zip files');
 
     var zip = new JSZip();
-    for(var i = 0; i < imageArray.length; i++) {
+    for(var i = start; i < end; i++) {
         if(imageArray[i]) {
             console.log(`[MelonRipper] Packing image ${(i+1)} of ${imageArray.length}`);
-            zip.file((i+1) + '.png', imageArray[i], {base64: true});
+            zip.file((i+1).toString().padStart(4, '0') + '.png', imageArray[i], {base64: true});
         } else {
             console.log(`[MelonRipper] Skipping image ${(i+1)} of ${imageArray.length}`);
         }
@@ -94,7 +118,7 @@ browser.runtime.onMessage.addListener((message) => {
             status: 'saving'
         });
         isSaving = true;
-        saveToZip();
+        saveToZip(tenth, pageTotal);
         console.log('[MelonRipper] Saving zip of book');
     }
 });
